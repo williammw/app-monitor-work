8c85d0007e70a7fd496b8ae6e96b4c7f2a0aa96f
wwwwwmw <william.manwai@gmail.com>
2023-01-15 23:24:53 +0800

    redo again

---
+++ src/components/DashboardLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function DashboardLayout() {
+  return (
+    <div >
+      
+      <div className="flex">
+        <nav className="w-64 bg-white">
+          {/* Left navigation goes here */}
+          left
+        </nav>
+        <main className="flex-1 p-4">
+          {/* Main content goes here */}
+          main
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
