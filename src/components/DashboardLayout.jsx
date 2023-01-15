2ca93ef7f5992646db3c3fa09ca2c26592bae614
wwwwwmw <william.manwai@gmail.com>
2023-01-16 02:59:35 +0800

    scroll fixed

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -5,19 +5,41 @@ function DashboardLayout() {
   return (
    
       
-      <div className="h-screen flex ">
+      <div className="h-screen flex pt-[45px] ">
         
           {/* Left navigation goes here */}
-          <nav className="bg-gray-200 w-64 overflow-y-auto position-sticky top-0">
+          <nav className="w-64 overflow-y-auto position-sticky top-0">
             {[...Array(100)].map((_, i) => (
-              <a href="#" key={i} className="block p-3 text-gray-800 hover:bg-gray-300">
-                Item {i + 1}
-              </a>
+              <ul className='bg-gray-200 pl-3  hover:bg-gray-300'>
+                <li className='border-b-[0.5px] border-gray-700 '>
+                  <a href="#" key={i} className="block p-3 text-gray-800 ">
+                    Item {i + 1}
+                  </a>
+                </li>
+              </ul>
             ))}
           </nav>
-        <main className="flex-1 p-4">
+        <main className="flex-1 overflow-y-auto transform translate-y-50 ">
           {/* Main content goes here */}
-          main
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+        <div className='text-xl'>hello</div>
+
         </main>
       </div>
   
