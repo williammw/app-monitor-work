28d72361b8e337a83c567bb370d8d77f35200887
William Wong <wmwwong@legco.gov.hk>
2023-01-16 09:01:59 +0800

    staging

--- src/components/MyReactComponent.jsx
+++ src/components/MyReactComponent.jsx
@@ -1,34 +1,40 @@
 
 import React, {useEffect, useState} from 'react'
 import './style.css'
+import CircularProgress from '@mui/material/CircularProgress';
+
+
 function MyReactComponent({initial}) {
   const [data, setData] = useState(null)
-   
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => { 
     console.log("useEffect")
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(json => setData(json))
-    
-
+      .then(json => setData(json))  
+    setIsLoading(false)
   },[])
+  // setup the component
   
-      // setup the component
-  
-
   return (
-    <div>
-      
+  <div>
+    {
+      isLoading? <CircularProgress />
+        :
+      <div>      
       <div>MyReactComponent</div>
       <div className="miji">
         {data && data.map((item, index) => (
           <div key={index}>
             <div>{item.name}</div>
             <div>{item.username}</div>
-            <div>{initial}</div>
+            <div>------</div>
           </div>
         ))}
       </div>
+      </div>   
+    }
     </div>
   )
 }
