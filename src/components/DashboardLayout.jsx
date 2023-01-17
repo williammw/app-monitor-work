b7e2661624c3f80da17e5e4707df5cfdcb61feea
wwwwwmw <william.manwai@gmail.com>
2023-01-18 00:11:00 +0800

    layout

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -2,20 +2,8 @@ import React from "react";
 
 function DashboardLayout() {
   return (
-    <div className="h-screen flex pt-[45px] ">
-      {/* Left navigation goes here */}
-      <nav className="w-64 overflow-y-auto position-sticky top-0 hidden sm:block">
-        {[...Array(100)].map((_, i) => (
-          <ul className="bg-gray-200 pl-3  hover:bg-gray-300">
-            <li className="border-b-[0.5px] border-gray-700 ">
-              <a href="#" key={i} className="block p-3 text-gray-800 ">
-                Item {i + 1}
-              </a>
-            </li>
-          </ul>
-        ))}
-      </nav>
-      <main className="flex-1 overflow-y-auto transform translate-y-50 ">
+    <div className="flex w-[100%]">
+      <main className="flex-1 overflow-y-auto transform translate-y-50 pl-3 ">
         {/* Main content goes here */}
         {[...Array(100)].map((_, i) => (
           <div key={i} className="text-xl">
---
+++ src/components/Icon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid";
+
+function Icon() {
+  return (
+    <AdjustmentsHorizontalIcon className=" h-6 w-6 text-white cursor-pointer  " />
+  );
+}
+
+export default Icon;
